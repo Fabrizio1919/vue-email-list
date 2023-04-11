52a3e53e9e9760923af807513c9d2a14b614f8cf
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-04-11 15:17:06 +0200

    Generate mail in page

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            title: 'Vue Email List',
+            visible: false,
+            apiRandomMail: "https://flynn.boolean.careers/exercises/api/random/mail",
+            mailNumber: 10,
+            mailList: []
+        }
+    },
+    methods: {
+        // FUNZIONE CHE VISUALIZZA LA LISTA 
+        showList() {
+            if (this.mailList.length === this.mailNumber) {
+                this.visible = true;
+            }
+        },
+
+        // FACCIO UNA RICHESTA AL SERVER E LA INSERISCO IN UN ARRAY
+        request(indirizzo, array) {
+            axios.get(indirizzo)
+                .then((response) => {
+                    array.push(response.data.response)
+                    // MOSTRA LA LISTA DEI CONTATTI SOLO QUANDO LI CARICA TUTTI 
+                    this.showList()
+                })
+        },
+        // ESTRAI DATI DA UN API X NUMERO DI VOLTE
+        repeatRequest(number) {
+            for (let i = 0; i < number; i++) {
+                this.request(this.apiRandomMail, this.mailList)
+            }
+        },
+    },
+    beforeMount() {
+        // PRIMA CHE IL PROGRAMMA VENGA MONTATO ESEGUI QUESTO METODO 
+        this.repeatRequest(this.mailNumber)
+
+    }
+
+
+}).mount('#app')
